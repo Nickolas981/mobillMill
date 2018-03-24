057412d74e01a5fa1de6333b6df2ce37138fa773
Nickolas981 <kolin98111@gmail.com>
2018-03-24 21:08:54 +0200

    Moved Weather Entity

---
+++ app/src/main/java/com/example/ngumeniuk/mobillmill/weatherFragment/data/models/databaseEnteties/WeatherModel.kt
@@ -0,0 +1,17 @@
+package com.example.ngumeniuk.mobillmill.weatherFragment.data.models.databaseEnteties
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import com.example.ngumeniuk.curogram.utils.Differeble
+
+@Entity
+data class WeatherModel(
+        val temp: Double,
+        val tempMin: Double,
+        val tempMax: Double,
+        val dt: String,
+        @PrimaryKey(autoGenerate = true)
+        var id: Int = 0
+) : Differeble{
+        override fun getIdToDiff(): Int = id
+}
