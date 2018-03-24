3203d15ca3ef95ac5f4a7307687c83e06de8a2bd
Nickolas981 <kolin98111@gmail.com>
2018-03-24 20:56:20 +0200

    Created weather list adapter

---
+++ app/src/main/java/com/example/ngumeniuk/mobillmill/weatherFragment/adapters/WeatherListAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.ngumeniuk.mobillmill.weatherFragment.adapters
+
+import android.annotation.SuppressLint
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ngumeniuk.mobillmill.R
+import com.example.ngumeniuk.mobillmill.weatherFragment.data.models.WeatherModel
+import com.example.ngumeniuk.mobillmill.widgets.adapters.ListAdapter
+import kotlinx.android.synthetic.main.item_weather_list.view.*
+
+
+class WeatherListAdapter : ListAdapter<WeatherModel, WeatherListAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int)=
+            holder.bind(list[position])
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+            ViewHolder(parent.inflate(R.layout.item_weather_list))
+
+    private fun ViewGroup.inflate(layoutRes: Int): View =
+            LayoutInflater.from(context).inflate(layoutRes, this, false)
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        @SuppressLint("SetTextI18n")
+        fun bind(model: WeatherModel) {
+            with(itemView) {
+                with(model){
+                    dateTV.text = dt
+                    tempTV.text = temp.toString() + "°"
+                    min_and_maxTV.text = "$tempMin° - $tempMax°"
+                }
+            }
+        }
+    }
+}
