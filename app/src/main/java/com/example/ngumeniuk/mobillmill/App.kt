52a426039f020c4649862feed750f896ddcef4d9
Nickolas981 <kolin98111@gmail.com>
2018-03-24 21:31:21 +0200

    Created App class

---
+++ app/src/main/java/com/example/ngumeniuk/mobillmill/App.kt
@@ -0,0 +1,22 @@
+package com.example.ngumeniuk.mobillmill
+
+import android.app.Application
+import com.example.ngumeniuk.mobillmill.location.LocationModule
+import com.example.ngumeniuk.mobillmill.weatherFragment.di.DaggerWeatherComponent
+import com.example.ngumeniuk.mobillmill.weatherFragment.di.WeatherComponent
+
+class App : Application(){
+    companion object {
+        lateinit var weatherComponent: WeatherComponent
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        weatherComponent = buildWeatherComponent()
+    }
+
+    private fun buildWeatherComponent() = DaggerWeatherComponent
+            .builder()
+            .locationModule(LocationModule(applicationContext))
+            .build()
+}
