f38a5389a10d250da2ba03261d9b98e059fe249c
Nickolas981 <kolin98111@gmail.com>
2018-03-24 21:28:00 +0200

    created weather view model

---
+++ app/src/main/java/com/example/ngumeniuk/mobillmill/weatherFragment/viewModel/WeatherViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.ngumeniuk.mobillmill.weatherFragment.viewModel
+
+import com.example.ngumeniuk.curogram.utils.BaseViewModel
+
+
+class WeatherViewModel : BaseViewModel() {
+
+
+
+
+}
