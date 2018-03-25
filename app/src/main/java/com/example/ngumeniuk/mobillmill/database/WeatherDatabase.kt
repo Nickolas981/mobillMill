0689d1c643634eeaf23042b818b4b8dc2a82cc68
Nickolas981 <kolin98111@gmail.com>
2018-03-25 22:30:02 +0300

    Moved database class

--- app/src/main/java/com/example/ngumeniuk/mobillmill/database/WeatherDatabase.kt
+++ app/src/main/java/com/example/ngumeniuk/mobillmill/database/WeatherDatabase.kt
@@ -1,9 +1,9 @@
-package com.example.ngumeniuk.mobillmill.weatherFragment.data.database
+package com.example.ngumeniuk.mobillmill.database
 
 import android.arch.persistence.room.Database
 import android.arch.persistence.room.RoomDatabase
-import com.example.ngumeniuk.mobillmill.weatherFragment.data.dao.WeatherDao
-import com.example.ngumeniuk.mobillmill.weatherFragment.data.models.databaseEnteties.WeatherModel
+import com.example.ngumeniuk.mobillmill.weatherFragment.logic.data.dao.WeatherDao
+import com.example.ngumeniuk.mobillmill.weatherFragment.logic.data.models.databaseEnteties.WeatherModel
 
 @Database(entities = arrayOf(WeatherModel::class), version = 1, exportSchema = false)
 abstract class WeatherDatabase : RoomDatabase() {
